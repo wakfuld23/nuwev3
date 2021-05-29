0d2aab63e61889270ec90ec0b4838cee574cd41b
abelfubu <abelfubu@gmail.com>
2021-05-29 17:12:37 +0200

    fix: vite config build folder

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,5 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    outDir: 'dist',
-  },
   plugins: [reactRefresh(), tsconfigPaths()],
 })
