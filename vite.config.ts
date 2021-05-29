1aca368bcd8442bf520775cb7dc0d68e2166467c
abelfubu <abelfubu@gmail.com>
2021-05-29 17:03:06 +0200

    fix: output build folder for gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    outDir: 'docs',
+    outDir: 'dist',
   },
   plugins: [reactRefresh(), tsconfigPaths()],
 })
