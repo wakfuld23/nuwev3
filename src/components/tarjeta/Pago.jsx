d412b5117ca28b9ca942eee493c2267afd638916
Alex Declercq <lalal_la@hotmail.com>
2021-05-30 12:29:50 -0300

    commits

--- src/components/tarjeta/Pago.jsx
+++ src/components/tarjeta/Pago.jsx
@@ -3,15 +3,52 @@ import React from 'react'
 const Pago = () => {
 
 const imgUrl = "https://images.pexels.com/photos/4968633/pexels-photo-4968633.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+const datos = { id, nombre, creditos }
+    
+     constructor(props, context) {
+        super(props, context);
 
+        this.state = { datos };
+    }
+
+    onChange(e) {
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+
+        fetch(this.props.formAction, {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({description: this.state.description})
+        });
+
+        this.setState({description: ''});
+    }
+    
+    
     return (
         <div className="container">
             <h1 className="text-center mt-sm-4 mb-sm-5 mt-3">Paga con Tarjeta</h1>
             <div className="row mt-sm-4 mt-3">
-                <div className="col-12 col-sm-6 mb-4">
-                    <img src={imgUrl} alt="img-tarjeta" className="img-fluid" />
+                <div className="card col-sm-6 col-12 pt-sm-5 py-5 mb-4" style={{background: 'lightgray' }}>
+                   <div className="card-body">
+                       <h5># de compra (id):</h5>
+                     <h2 className="card-title">Has comprado:</h2>
+                     <h3 className="card-text">Tienes ...  creditos.</h3>
+                   </div>
                 </div>
-                <form className="col-12 col-sm-6 my-auto">
+
+                <form id="main-login"
+                    //   action={this.props.action}
+                      method={this.props.method}
+                      onSubmit={this.onSubmit}
+                      className="col-12 col-sm-6 my-auto">
                     <label className="form-label">Nombre y Apellido del titular:</label>
                     <input type="text" className="form-control mb-sm-2 mb-3"/>
                     <label>Número de Tarjeta:</label>
@@ -27,6 +64,7 @@ const imgUrl = "https://images.pexels.com/photos/4968633/pexels-photo-4968633.jp
                           <input type="text" className="form-control"/>
                        </div>
                     </div>
+                    <button className="btn btn-danger btn-block">Comprar</button>
                 </form>
             </div>
         </div>
